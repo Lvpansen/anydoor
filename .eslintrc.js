65847881618218b06a6f5b3dbb1add01a3500291
Lvps <805697518@qq.com>
2018-06-10 23:04:41 +0800

    init

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,14 +2,16 @@ module.exports = {
   "extends": ["eslint:recommended"],
   "parser": "babel-eslint",
   "rules": {
-    "no-console":"warn"
+    "no-console":["warn",{
+      "allow":["info","error"]
+    }]
   },
   "parserOptions": {
     "ecmaVersion": 6,
     "sourceType": "script"
   },
   "globals": {
-    //"window": true
+    "window": true
   },
   "env": {
     "node": true,
